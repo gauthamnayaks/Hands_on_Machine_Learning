845c261665f39f4194acbccba14c2131c8b8681d
gauthamnayaks <gauthamnayaks@gmail.com>
2020-02-04 14:40:55 +0100

    chapter 4 init

---
+++ Book_Examples/4_training_models.py
@@ -0,0 +1,85 @@
+# Python ≥3.5 is required
+import sys
+assert sys.version_info >= (3, 5)
+
+# Scikit-Learn ≥0.20 is required
+import sklearn
+assert sklearn.__version__ >= "0.20"
+
+# Common imports
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+
+# To plot pretty figures
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "training_linear_models"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+# Ignore useless warnings (see SciPy issue #5998)
+import warnings
+
+# %% Linear regression using Normal eqution
+
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.axis([0, 2, 0, 15])
+save_fig("generated_data_plot")
+plt.show()
+
+# %% Predict theta using Normal Equation
+
+X_b = np.c_[np.ones((100, 1)), X]  # add x0 = 1 to each instance
+theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+print(theta_best)
+
+# %% Use theta^ value to predict new values
+X_new = np.array([[0], [2]])
+X_new_b = np.c_[np.ones((2, 1)), X_new]  # add x0 = 1 to each instance
+y_predict = X_new_b.dot(theta_best)
+print(y_predict)
+
+plt.plot(X_new, y_predict, "r-", linewidth=2, label="Prediction")
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.legend(loc="upper left", fontsize=14)
+plt.axis([0, 2, 0, 15])
+save_fig("linear_model_predictions_plot")
+plt.show()
+
+
+# %% Linear Regression
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+print(lin_reg.intercept_, lin_reg.coef_)
+lin_reg.predict(X_new)
+
+theta_best_svd, residuals, rank, s = np.linalg.lstsq(X_b, y, rcond=1e-6)
+print(theta_best_svd)
+
+np.linalg.pinv(X_b).dot(y)
+
