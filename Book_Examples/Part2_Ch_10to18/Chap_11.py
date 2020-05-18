512ca1bb2034316dea73c099744ca8cb783179c9
gauthamnayaks <gauthamnayaks@gmail.com>
2020-05-18 22:18:09 +0200

    split into 2 parts

---
+++ Book_Examples/Part2_Ch_10to18/Chap_11.py
@@ -0,0 +1,140 @@
+# %% Import relevant libraries
+#import tensorflow as tf
+import os
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+
+# %% Import Fashion dataset from Keras
+fashion_mnist = keras.datasets.fashion_mnist
+(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+X_train_full.shape
+X_train_full.dtype
+
+# %% Split the dataset into training, test and validation
+X_valid, X_train = X_train_full[:5000] / 255., X_train_full[5000:] / 255.
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+X_test = X_test / 255.
+
+# %% Plot one of the images
+plt.imshow(X_train[250], cmap="binary")
+plt.axis('off')
+plt.show()
+
+# %% Just check some data
+y_train
+X_train.shape
+X_valid.shape
+X_test.shape
+y_train.shape
+y_valid.shape
+y_test.shape
+
+# %% Attach labels
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+
+# %% Initialize Keras with appropriate layers
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28, 28]))
+model.add(keras.layers.Dense(300, activation="relu"))
+model.add(keras.layers.Dense(100, activation="relu"))
+model.add(keras.layers.Dense(10, activation="softmax"))
+
+keras.backend.clear_session()
+np.random.seed(42)
+tf.random.set_seed(42)
+
+model.summary()
+keras.utils.plot_model(model, "my_fashion_mnist_model.png", show_shapes=True)
+hidden1 = model.layers[1]
+weights, biases = hidden1.get_weights()
+weights
+
+model.compile(loss="sparse_categorical_crossentropy", optimizer="sgd" , metrics=["accuracy"])
+
+# %% Train the model
+keras.backend.clear_session()
+history = model.fit(X_train, y_train, epochs=100, validation_data=(X_valid, y_valid))
+
+# %% Plot the learning curves
+import pandas as pd
+
+pd.DataFrame(history.history).plot(figsize=(8, 5))
+plt.grid(True)
+plt.gca().set_ylim(0, 1)
+plt.show()
+
+# %%
+model.evaluate(X_test, y_test)
+
+# %% Regression MLP
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+housing = fetch_california_housing()
+
+X_train_full, X_test, y_train_full, y_test = train_test_split(housing.data, housing.target, random_state=42)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_valid = scaler.transform(X_valid)
+X_test = scaler.transform(X_test)
+
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+# %% Build the model
+model = keras.models.Sequential([keras.layers.Dense(30, activation="relu", input_shape=X_train.shape[1:]), keras.layers.Dense(1)])
+model.compile(loss="mean_squared_error", optimizer=keras.optimizers.SGD(lr=1e-3))
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid))
+mse_test = model.evaluate(X_test, y_test)
+X_new = X_test[:3]
+y_pred = model.predict(X_new)
+
+# %% Plot the loss function
+plt.plot(pd.DataFrame(history.history))
+plt.grid(True)
+plt.gca().set_ylim(0, 1)
+plt.show()
+
+# %% Complex Neural network .wide and deep arch from 2016 heng-tze paper
+
+input_ = keras.layers.Input(shape=X_train.shape[1:])
+hidden1 = keras.layers.Dense(30, activation="relu")(input_)
+hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+concat = keras.layers.concatenate([input_, hidden2])
+output = keras.layers.Dense(1)(concat)
+model = keras.models.Model(inputs=[input_], outputs=[output])
+
+model.summary()
+
+model.compile(loss="mean_squared_error", optimizer=keras.optimizers.SGD(lr=1e-3))
+history = model.fit(X_train, y_train, epochs=20, validation_data=(X_valid, y_valid))
+mse_test = model.evaluate(X_test, y_test)
+y_pred = model.predict(X_new)
+
+# %%
+input_A = keras.layers.Input(shape=[5], name="wide_input")
+input_B = keras.layers.Input(shape=[6], name="deep_input")
+hidden1 = keras.layers.Dense(30, activation="relu")(input_B)
+hidden2 = keras.layers.Dense(30, activation="relu")(hidden1)
+concat = keras.layers.concatenate([input_A, hidden2])
+output = keras.layers.Dense(1, name="output")(concat)
+model = keras.models.Model(inputs=[input_A, input_B], outputs=[output])
+
+model.compile(loss="mse", optimizer=keras.optimizers.SGD(lr=1e-3))
+
+X_train_A, X_train_B = X_train[:, :5], X_train[:, 2:]
+X_valid_A, X_valid_B = X_valid[:, :5], X_valid[:, 2:]
+X_test_A, X_test_B = X_test[:, :5], X_test[:, 2:]
+X_new_A, X_new_B = X_test_A[:3], X_test_B[:3]
+
+history = model.fit((X_train_A, X_train_B), y_train, epochs=20, validation_data=((X_valid_A, X_valid_B), y_valid))
+mse_test = model.evaluate((X_test_A, X_test_B), y_test)
+y_pred = model.predict((X_new_A, X_new_B))
